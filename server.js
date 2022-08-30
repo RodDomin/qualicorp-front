af83e3bb41c0b21ba5df29d770714707c5e2b3e2
Rodrigo Alves <rodrigoalves009@gmail.com>
2022-08-30 19:36:59 -0300

    reverted server file

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const path = require('path');
 const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 
-const port = process.env.PORT || 3000;
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port);
-console.log(`server started ${port}`);
+console.log(`app is listening on port: ${port}`);
